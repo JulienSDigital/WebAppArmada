8a49cfb00db1a2c56d72e382c4bcc6fc937cf02e
Jean-Pierre Planas <sybaris@club-internet.fr>
2019-02-08 09:28:21 +0100

    first commit

---
+++ Armada/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Armada.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
